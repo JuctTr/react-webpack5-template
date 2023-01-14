0026b68adad55148997c6db6e1cecd5aefd9ac93
JuctTr <1301842163@qq.com>
2023-01-14 22:10:38 +0800

    feat(👻): context

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,35 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef, useCallback, useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Nothing, ReadComponent, WriteComponent } from './components/index';
 import Context from './context';
 import './index.scss';
 
 function Index(): React.ReactElement {
+    const [count, setCount] = useState(0);
+    useLayoutEffect(() => {
+        console.log('useLayoutEffect => ');
+        return () => {
+            console.log('useLayoutEffect Destroy => ');
+        };
+    });
+    useEffect(() => {
+        console.log('useEffect => ');
+        return () => {
+            console.log('useEffect Destroy => ');
+        };
+    }, []);
+    const addCount = () => {
+        setCount(pre => ++pre);
+    };
+
     return (
         <Context>
             <div className="page">Hello React + Webpack5 + TypeScript</div>
             <Nothing />
             <ReadComponent />
             <WriteComponent />
+            <div className="box">{count}</div>
+            <button onClick={addCount}>Add</button>
         </Context>
     );
 }
@@ -25,7 +44,7 @@ function Index(): React.ReactElement {
 //     }
 // }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 /**
  * @document https://zh-hans.reactjs.org/docs/strict-mode.html
